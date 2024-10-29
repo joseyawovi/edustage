8f15d7684989dc75c8bb978f66e5e3cc0033bd13
joseyawovi <joseyawovi@gmail.com>
2024-10-30 01:02:58 +0530

    navbar

--- classes/admin.py
+++ classes/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import Course, Outline, SubOutline,Registration, Category
 from account.models import CustomUser
 
+# main_app/admin.py
+from django.contrib import admin
+
+admin.site.site_header = "MySite Administration"
+admin.site.site_title = "MySite Admin Portal"
+admin.site.index_title = "Welcome to MySite Admin"
+
 class SubOutlineInline(admin.TabularInline):
     model = SubOutline
     extra = 1  # Number of empty forms shown initially
